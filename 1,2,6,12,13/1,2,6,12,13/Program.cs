c65951b1d469f5b5474b2023b87b163ea4770601
MotyaAbormotya <76623037+MotyaAbormotya@users.noreply.github.com>
2021-03-18 11:24:27 +0300

    Add files via upload

---
+++ 1,2,6,12,13/1,2,6,12,13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_2_6_12_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Cut(ref text);
+            Console.ReadKey();
+        }
+        public static void Cut(ref string text)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int minIndex = int.MaxValue;
+            int maxIndex = int.MinValue;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]) == true)
+                {
+                    if (text[i] > max)
+                    {
+                        max = text[i];
+                        maxIndex = i;
+                    }
+                }
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]) == true)
+                {
+                    if (text[i] < min)
+                    {
+                        min = text[i];
+                        minIndex = i;
+                    }
+                }
+            }
+            text = text.Remove(maxIndex, 1);
+            text = text.Remove(minIndex, 1);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ 1,2,6,12,13/12/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] text = new int[3, 5];
+            Random random = new Random();
+
+            for (int i = 0; i < text.GetLength(0); i++)
+            {
+                for (int j = 0; j < text.GetLength(1); j++)
+                {
+                    text[i, j] = random.Next(1, 15 + 1);
+                    Console.Write(text[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Arifmetic(text);
+            Console.ReadKey();
+        }
+
+        public static void Arifmetic(int[,] text)
+        {
+            int result = 0;
+            int index = 1;
+            int arifmetic = 0;
+
+            for (int i = 0; i < text.GetLength(0); i++)
+            {
+                arifmetic += text[0, i];
+            }
+
+            arifmetic /= text.GetLength(1);
+
+            for (int i = 1; i < text.GetLength(0); i++)
+            {
+                for (int j = 0; j < text.GetLength(1); j++)
+                {
+                    result += text[i, j];
+                }
+                result /= text.GetLength(0);
+                if (arifmetic < result)
+                {
+                    arifmetic = result;
+                    index = i + 1;
+                }
+                result = 0;
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}
